588bc8f6d4cdd835cb91211e89f3b2f9255025ad
jelcacho <jelcacho@nearcrumbs.com>
2018-04-11 13:16:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
  echo "<p>JoaquimPhpWebApp</p>";
   
  echo "Hola soc el servidor: " .gethostname();
- #" Em vull connectar al servidor $servername"; 
+ echo "Em vull connectar al servidor $servername"; 
  #echo '<p>Em vull connectar al servidor $servername </p>';
  ?>
  
