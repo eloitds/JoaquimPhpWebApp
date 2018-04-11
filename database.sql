ab9e4fb71cc9a73f6d9edf3c2233fbdb5f1ae377
jelcacho <jelcacho@nearcrumbs.com>
2018-04-11 12:56:06 +0200

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE `joaquimphpwebapp` /*!40100 DEFAULT CHARACTER SET latin1 */;
+use joaquimphpwebapp;
+CREATE TABLE `connectionlog` (
+  `server` varchar(45) NOT NULL,
+  `ip` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
